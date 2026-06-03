e6969640304373126f6cbbd98121d27ca803d395
Kyame Israel <kyame.israel@gmail.com>
2026-06-03 02:26:55 -0400

    feat: setup my Unit 1 directory

---
+++ python/unit_01/runner.py
@@ -0,0 +1,5 @@
+# Problem 1:
+# Write a function linear_search() to help Winnie the Pooh locate his lost items. 
+# The function accepts a list items and a target value as parameters. The function should 
+# return the first index of target in items, and -1 if target is not in items. Do not use any built-in functions.
+
