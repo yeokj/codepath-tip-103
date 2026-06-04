6c0c87a8a9c2db3127950eeb1931c8565bcc015d
Kyame Israel <kyame.israel@gmail.com>
2026-06-03 21:33:45 -0400

    feat: completed problem 2

--- python/unit_01/runner.py
+++ python/unit_01/runner.py
@@ -1,6 +1,6 @@
 from solutions import Solutions
 
-# Problem 1:
+# Problem 1, Set 1:
 # Write a function linear_search() to help Winnie the Pooh locate his lost items. 
 # The function accepts a list items and a target value as parameters. The function should 
 # return the first index of target in items, and -1 if target is not in items. Do not use any built-in functions.
@@ -16,3 +16,21 @@ print(Solutions.linear_search(items, target))
 # Output:
 # 3
 # -1
+
+# Problem 2, Set 1:
+# Tigger has developed a new programming language Tiger with only four operations and one variable tigger.
+
+# bouncy or flouncy both increment the value of the variable tigger by 1.
+# trouncy or pouncy both decrement the value of the variable tigger by 1.
+# Initially, the value of tigger is 1 because he's the only tigger around! Given a list of strings operations 
+# containing a list of operations, return the final value of tigger after performing all the operations.
+
+operations = ["trouncy", "flouncy", "flouncy"]
+print(Solutions.final_value_after_operations(operations))
+
+operations = ["bouncy", "bouncy", "flouncy"]
+print(Solutions.final_value_after_operations(operations))
+
+# Output:
+# 2
+# 4
--- python/unit_01/solutions.py
+++ python/unit_01/solutions.py
@@ -4,4 +4,15 @@ class Solutions:
         for i in range(len(items)):
             if items[i] == target:
                 return i
-        return -1 # Space: O(1), Time O(N)
+        return -1 # Space: O(1), Time: O(N)
+    
+    @staticmethod
+    def final_value_after_operations(operations):
+        tigger = 1
+
+        for operation in operations:
+            if operation == "bouncy" or operation == "flouncy":
+                tigger += 1
+            else:
+                tigger -= 1
+        return tigger # Space: O(1), Time: O(N)
